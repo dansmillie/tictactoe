61ec4d551df8234706680f9288dd576ab3f5bbab
Dan Smillie <dansmillie96@gmail.com>
2017-01-27 11:15:48 -0500

    initial commit

---
+++ TicTacToe/app/src/main/java/com/example/dansmillie/tictactoe/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dansmillie.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TableLayout;
+import android.
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int SIZE = 9;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
